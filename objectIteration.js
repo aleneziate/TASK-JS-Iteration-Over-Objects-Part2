dfc916fbb4138623348f358ec647d3a5d246b7b9
alene <alene@ABDALLAH>
2025-02-13 13:53:13 +0300

    solved

--- objectIteration.js
+++ objectIteration.js
@@ -54,19 +54,25 @@ const students = [
 // 1) Write a `getStudentName` function that accepts an argument of `student` object, return the student name
 function getStudentName(student) {
   // write your code here...
+  return student.name;
 }
 // console.log(getStudentName(students[0]))
 
 // 2) Write a `getCourse` function that accepts a `student` object and `courseIndex` return the course at the specified course index in the student's courses array
 function getCourse(student, courseIndex) {
   // write your code here...
+  return student.courses[courseIndex];
 }
+gi
 // console.log(getCourse(students[4], 2)); // Outputs: Music
 
 // 3) Write a `addCourseToStudent` function that accepts a `student` object and `course` string,
 // it will add the course to the student's courses array and return the `student` object
 function addCourseToStudent(student, course) {
   // write your code here...
+   student.courses.push(course);
+  return student;
+  
 }
 // console.log(addCourseToStudent(students[7], "Physics"));
 
@@ -74,6 +80,7 @@ function addCourseToStudent(student, course) {
 // then returns the number of courses the student is enrolled in
 function countCourses(student) {
   // write your code here...
+  return student.courses.length;
 }
 // console.log(countCourses(students[1])); // Outputs: 4
 
@@ -82,6 +89,9 @@ function countCourses(student) {
 // then returns the `student` object.
 function removeCourseFromStudent(student, course) {
   // write your code here...
+    student.courses = student.courses.filter(c => c !== course);
+  return student;
+
 }
 // console.log(removeCourseFromStudent(students[6], "Science"));
 
@@ -89,6 +99,8 @@ function removeCourseFromStudent(student, course) {
 // and returns the student object with the matching id.
 // It should return undefinded if a student is not found
 function findStudentById(students, studentId) {
+    return students.find(student => student.id === studentId);
+
   // write your code here...
 }
 // console.log(findStudentById(students, 10));
@@ -97,6 +109,8 @@ function findStudentById(students, studentId) {
 // 7) Write a `getStudentsByCourse` function that accepts a `course` string and an array of student objects `students`,
 // then returns an array of student objects who are enrolled in the specified course
 function getStudentsByCourse(students, course) {
+    return students.filter(student => student.courses.includes(course));
+
   // write your code here...
 }
 // console.log(getStudentsByCourse(students, "Music"));
@@ -105,6 +119,13 @@ function getStudentsByCourse(students, course) {
 // 8) Write a `listAllCourses` function that accepts an array of `students`
 // then returns an array of all unique courses names across all students
 function listAllCourses(students) {
+   const allCourses = new Set();
+  students.forEach(student => {
+    student.courses.forEach(course => {
+      allCourses.add(course);
+    });
+  });
+  return Array.from(allCourses);
   // write your code here...
 }
 // console.log(listAllCourses(students));
